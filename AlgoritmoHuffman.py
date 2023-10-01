030bbbbc5ead10b81cda18a322e3897f60e22ba6
João M. Diniz <95003579+JoaoMarceloDMSilva@users.noreply.github.com>
2023-10-01 12:02:55 -0300

    AlgoritmoHuffman.py
    Atualização 03

--- AlgoritmoHuffman.py
+++ AlgoritmoHuffman.py
@@ -1,10 +1,9 @@
 nodoNumber = 1  #   Identificar os "Nodo" gerados por "createNodoByTuple"
 
-#   ALTERAR
 class Nodo:
-    def __init__(self, name: str) -> None:  #   <-- Aqui
+    def __init__(self, name: str, value: int) -> None:
         self.name = name
-        self.value = None   # <-- Aqui
+        self.value = value
         self.left = None
         self.right = None
 
@@ -34,7 +33,7 @@ def listWhitoutRepetition(phrase: list) -> list:
         return [head(phrase)] + listWhitoutRepetition(deleteChar(head(phrase), tail(phrase)))
 
 #   Tranforma uma lista de Char em um Dicionário;
-def listToDict(phrase: list) -> dict:
+def listToDict(phrase: list) -> dict:   #   Usado apenas para criar o Dicionário Original
     auxDict = dict()
     for key in listWhitoutRepetition(phrase):   #   Chaves únicas
         auxDict[key] = phrase.count(key)        #   Conta as aparições desse Char 
@@ -55,7 +54,7 @@ def quicksortDict(initialDictionary: dict) -> dict:
         #   Desempacotar (**) os dicionários. Semelhante a um [] + []
         return {**quicksortDict(larger), **equals, **quicksortDict(smaller)}
 
-#   Transforma Dict >> List(Dict);
+#   Separa um Dicionário em uma Lista de Dicionários;
 def dictToList(dictionary: dict) -> list:
     auxList = []
     for key in dictionary.keys():
@@ -64,49 +63,58 @@ def dictToList(dictionary: dict) -> list:
 
 #   Cria uma Tupla de Nodo para utilizar em "createNodoByTuple";
 def createNodoByList(listOfDict: list) -> tuple:
-    nodoLeft = Nodo(list(listOfDict[-1].keys())[0])
-    nodoRight = Nodo(list(listOfDict[-2].keys())[0])
+    nodoLeft = Nodo(list(listOfDict[-1].keys())[0], list(listOfDict[-1].values())[0])
+    nodoRight = Nodo(list(listOfDict[-2].keys())[0], list(listOfDict[-2].values())[0])
     return (nodoLeft, nodoRight)
 
 #   Cria um "Nodo" Pai cujos filhos são os elementos da Tupla
 def createNodoByTuple(tupleWithObj: tuple) -> Nodo:
     global nodoNumber
-    auxNodo = Nodo(f"N{nodoNumber}")
+    auxNodo = Nodo(f"N{nodoNumber}", tupleWithObj[0].value + tupleWithObj[1].value)
     nodoNumber += 1
     auxNodo.addRight(tupleWithObj[0])
     auxNodo.addLeft(tupleWithObj[1])
     return auxNodo
 
 #   List(Dict) >> Dict para poder reorganizar em "quicksortDict"
-def dictToSort(listDict: list) -> dict:
+def createDictToSort(listDict: list) -> dict:
     return {key: value  for i in listDict for key, value in i.items()}
 
 #   Adiciona um dict(Nodo) na List(Dict)
 def addNodoToListofDict(nodo: Nodo, listWithDict: list)-> list:
-    listWithDict.append({nodo.name: 1})
+    listWithDict.append({nodo.name: nodo.value})
     return listWithDict
 
-#   Organiza a nova List(Dict)
+def cleanListOfDict(listOfDictAfterAddNodo: list) -> list:
+    listAux = listOfDictAfterAddNodo.copy()
+    while len(listOfDictAfterAddNodo) != (len(listAux)-2):
+        del listOfDictAfterAddNodo[-1]
+    return listOfDictAfterAddNodo
+
+#   Organiza a Nova List(Dict)
 def newOrganizedDict(listWithDict: list) -> dict:
-    auxDict = dictToSort(listWithDict)
+    auxDict = createDictToSort(listWithDict)
     return quicksortDict(auxDict)
+
 
 #   O.B.S.: 
 #   I :: Precisa de um dicionário Auxiliar [remover os dois últimos elementos sempre que criar um Nodo] e um Original
-#   II :: O valor de um nodo deve ser igual a somo de seus filhos
 
 #def huffmanAlgorithm(phrase: str) -> dict:...
 
-
 # userText: str = "Joao Marcelo"
 
-# listaDicio =(dictToList(quicksortDict(listToDict(list(userText)))))
-# print(dictToSort(listaDicio))
-# nodoTuple = createNodoByList(dictToList(quicksortDict(listToDict(list(userText)))))
-# newNodo = createNodoByTuple(nodoTuple)
-
-# addNodo = addNodoToListofDict(newNodo, listaDicio)
-# print(f"Novo Dicionário:\n{newOrganizedDict(addNodo)}")
-# print(f"Nome: {newNodo.name}, Esquerda: {newNodo.left.name}, Direita: {newNodo.right.name}")
-# for i in nodoTuple:
-#     print(i.name)
+# passo01 = list(userText)
+# passo02 = listToDict(passo01)   # Transforma uma Lista em Dicionário
+# passo03 = quicksortDict(passo02) #  Dicionário Organizado Crescente
+# passo04 = dictToList(passo03)   # Transforma o Dicionário Organizado em uma Lista de Dicionários Organizado
+# #   LOOP
+# passo05 = createNodoByList(passo04) # Tranforma os dois últimos elementos em um Tupla de Nodo
+# passo06 = createNodoByTuple(passo05) # Cria um Nodo pai para a Tupla de Nodo
+# passo07 = addNodoToListofDict(passo06, passo04) # Adiciona o Nodo Pai no final da Lista de Dicionários Organizado
+# passo08 = createDictToSort(passo07) # Transforma a lista em Dicionário
+# passo09 = quicksortDict(passo08) # Organiza o novo Nodo
+# passo10 = dictToList(passo09)   # Transforma o Dicionário em Lista 
+# passo11 = cleanListOfDict(passo10) # Elimina os dois últimos elementos da lista
+# print(passo11)
+# #   END LOOP
