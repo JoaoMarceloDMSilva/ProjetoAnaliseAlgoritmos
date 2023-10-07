b32e431e9a7aa721cf7951e97289f899513af50c
João M. Diniz <95003579+JoaoMarceloDMSilva@users.noreply.github.com>
2023-10-07 12:45:27 -0300

    AlgoritmoHuffman.py
    Agora consegue criar os dicionários.
    Falta: Criar a lista binária e o processo de decriptação

--- AlgoritmoHuffman.py
+++ AlgoritmoHuffman.py
@@ -1,120 +1,92 @@
-nodoNumber = 1  #   Identificar os "Nodo" gerados por "createNodoByTuple"
-
+nodoNum = 1
 class Nodo:
     def __init__(self, name: str, value: int) -> None:
         self.name = name
         self.value = value
         self.left = None
         self.right = None
-
-    def addLeft(self, nodo: object):
+    def add_Left(self, nodo: object):
         self.left = nodo
-    
-    def addRight(self, nodo: object):
+    def add_Right(self, nodo: object):
         self.right = nodo
 
-def head(stringList: list):
-    return stringList[0] if len(stringList) > 0 else []
+class Leaf(Nodo):
+    def __init__(self, name: str, value: int) -> None:
+        super().__init__(name, value)
 
-def tail(stringList: list):
-    return stringList[1:] if len(stringList) > 0 else []
+#   Remove caracteres repetidos:
+def remove_duplicates(string: str):
+    return list(set(string))
 
-#   Deleta todas a aparições de um Char;
-def deleteChar(character: str, phrase: list) -> list:
-    while character in phrase:
-        phrase.remove(character)
-    return phrase
+#   Criar as Folhas da Árvore:
+def create_Leaves(phrase: str):
+    listWithoutDuplicates = remove_duplicates(phrase)
+    listWithLeaves = []
+    for char in listWithoutDuplicates:
+        aux = Leaf(char, phrase.count(char))
+        listWithLeaves.append(aux)
+    return listWithLeaves
 
-#   Cria uma lista sem Char repetidos;
-def listWhitoutRepetition(phrase: list) -> list:
-    if len(phrase) == 0:
-        return []
-    else:
-        return [head(phrase)] + listWhitoutRepetition(deleteChar(head(phrase), tail(phrase)))
+#   Organiza uma lista de Objetos Nodo em ordem decrescente
+def quicksort_decreasing(lista):
+    if len(lista) <= 1:
+        return lista
 
-#   Tranforma uma lista de Char em um Dicionário;
-def listToDict(phrase: list) -> dict:   #   Usado apenas para criar o Dicionário Original
-    auxDict = dict()
-    for key in listWhitoutRepetition(phrase):   #   Chaves únicas
-        auxDict[key] = phrase.count(key)        #   Conta as aparições desse Char 
-    return auxDict
+    pivot = lista[len(lista) // 2]
+    left = [x for x in lista if x.value > pivot.value]  # Elementos maiores que o pivô
+    middle = [x for x in lista if x.value == pivot.value]  # Elementos iguais ao pivô
+    right = [x for x in lista if x.value < pivot.value]  # Elementos menores que o pivô
 
-#   Organiza o Dicionário em Ordem Decrescente;
-def quicksortDict(initialDictionary: dict) -> dict:
-    if len(initialDictionary) <= 1:
-        return initialDictionary
-    else:
-        pivotKey = list(initialDictionary.keys())[0]    #   Pega a primeira chave do Dicionário
-        pivotValue = initialDictionary[pivotKey]        #   Pega o valor daquela chave
+    return quicksort_decreasing(left) + middle + quicksort_decreasing(right)
 
-        smaller = {key: value for key, value in initialDictionary.items() if value < pivotValue}
-        equals = {key: value for key, value in initialDictionary.items() if value == pivotValue}
-        larger = {key: value for key, value in initialDictionary.items() if value > pivotValue}
-
-        #   Desempacotar (**) os dicionários. Semelhante a um [] + []
-        return {**quicksortDict(larger), **equals, **quicksortDict(smaller)}
-
-#   Separa um Dicionário em uma Lista de Dicionários;
-def dictToList(dictionary: dict) -> list:
-    auxList = []
-    for key in dictionary.keys():
-        auxList.append({key: dictionary[key]})
+#   Ordena e Exclui os dois últimos itens da lista
+def rearrange(list_to_rearrange: list) -> list:
+    auxList = quicksort_decreasing(list_to_rearrange)
+    for i in range(2):
+        auxList.pop(-1)
     return auxList
 
-#   Cria uma Tupla de Nodo para utilizar em "createNodoByTuple";
-def createNodoByList(listOfDict: list) -> tuple:
-    nodoLeft = Nodo(list(listOfDict[-1].keys())[0], list(listOfDict[-1].values())[0])
-    nodoRight = Nodo(list(listOfDict[-2].keys())[0], list(listOfDict[-2].values())[0])
-    return (nodoLeft, nodoRight)
+def create_Tree(leaves: list):
+    if len(leaves) == 1:
+        return leaves
+    else:
+        global nodoNum
+        left_leaf = leaves[-1]
+        right_leaf = leaves[-2]
 
-#   Cria um "Nodo" Pai cujos filhos são os elementos da Tupla
-def createNodoByTuple(tupleWithObj: tuple) -> Nodo:
-    global nodoNumber
-    auxNodo = Nodo(f"N{nodoNumber}", tupleWithObj[0].value + tupleWithObj[1].value)
-    nodoNumber += 1
-    auxNodo.addRight(tupleWithObj[0])
-    auxNodo.addLeft(tupleWithObj[1])
-    return auxNodo
+        newNodo = Nodo(f"N{nodoNum}", left_leaf.value + right_leaf.value)
+        nodoNum += 1
 
-#   List(Dict) >> Dict para poder reorganizar em "quicksortDict"
-def createDictToSort(listDict: list) -> dict:
-    return {key: value  for i in listDict for key, value in i.items()}
+        newNodo.add_Left(left_leaf)
+        newNodo.add_Right(right_leaf)
 
-#   Adiciona um dict(Nodo) na List(Dict)
-def addNodoToListofDict(nodo: Nodo, listWithDict: list)-> list:
-    listWithDict.append({nodo.name: nodo.value})
-    return listWithDict
+        leaves.append(newNodo)
+        newList = rearrange(leaves)
 
-def cleanListOfDict(listOfDictAfterAddNodo: list) -> list:
-    listAux = listOfDictAfterAddNodo.copy()
-    while len(listOfDictAfterAddNodo) != (len(listAux)-2):
-        del listOfDictAfterAddNodo[-1]
-    return listOfDictAfterAddNodo
+        return create_Tree(newList)
 
-#   Organiza a Nova List(Dict)
-def newOrganizedDict(listWithDict: list) -> dict:
-    auxDict = createDictToSort(listWithDict)
-    return quicksortDict(auxDict)
+def huffman_codes(root: Nodo) -> dict:
+    codes = {}
+
+    def traverse(node, code):
+        if node.name is not None and isinstance(node, Leaf):
+            codes[node.name] = code
+        if node.left is not None:
+            traverse(node.left, code + '0')
+        if node.right is not None:
+            traverse(node.right, code + '1')
+    
+    traverse(root, '')
+    return codes
 
 
-#   O.B.S.: 
-#   I :: Precisa de um dicionário Auxiliar [remover os dois últimos elementos sempre que criar um Nodo] e um Original
+#   EXECUTAR
+lista_Folhas_Organizadas = quicksort_decreasing(create_Leaves("Joao Marcelo"))
 
-#def huffmanAlgorithm(phrase: str) -> dict:...
-
-# userText: str = "Joao Marcelo"
-
-# passo01 = list(userText)
-# passo02 = listToDict(passo01)   # Transforma uma Lista em Dicionário
-# passo03 = quicksortDict(passo02) #  Dicionário Organizado Crescente
-# passo04 = dictToList(passo03)   # Transforma o Dicionário Organizado em uma Lista de Dicionários Organizado
-# #   LOOP
-# passo05 = createNodoByList(passo04) # Tranforma os dois últimos elementos em um Tupla de Nodo
-# passo06 = createNodoByTuple(passo05) # Cria um Nodo pai para a Tupla de Nodo
-# passo07 = addNodoToListofDict(passo06, passo04) # Adiciona o Nodo Pai no final da Lista de Dicionários Organizado
-# passo08 = createDictToSort(passo07) # Transforma a lista em Dicionário
-# passo09 = quicksortDict(passo08) # Organiza o novo Nodo
-# passo10 = dictToList(passo09)   # Transforma o Dicionário em Lista 
-# passo11 = cleanListOfDict(passo10) # Elimina os dois últimos elementos da lista
-# print(passo11)
-# #   END LOOP
+raiz_Arvore = create_Tree(lista_Folhas_Organizadas)[0]
+dicionario = huffman_codes(raiz_Arvore)
+print(dicionario)
+# print(len(raiz_Arvore))
+# for item in lista_Folhas_Organizadas:
+#     print(f"{item.name}: {item.value}")
+# print(f"{raiz_Arvore.name}: {raiz_Arvore.value}")
