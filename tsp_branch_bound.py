8884e2e5b49d05d8763e3a61b9cb5ba4b62b52e3
João M. Diniz <95003579+JoaoMarceloDMSilva@users.noreply.github.com>
2023-11-19 12:06:26 -0300

    tsp_branch_bound.py
    Menor valor

--- tsp_branch_bound.py
+++ tsp_branch_bound.py
@@ -1,38 +1,28 @@
 import sys
-MAX_INT = sys.maxsize
 
-def criar_matriz(tamanho: int) -> list:
-    return [[0 for _ in range (tamanho)] for _ in range(tamanho)]
-
-
-def subtrair_valor_coluna(matriz: list, coluna: int, valor: int) -> list:
-    x = len(matriz)
-    y = len(matriz[0])
+def caixeiro_viajante(grafo: list, visitados: list, posicao_atual: int, quant_cidades: int, num_cidades_visitadas: int, custos: list, solucao_otima: list):
+    if (num_cidades_visitadas == quant_cidades) and grafo[posicao_atual][0]:
+        solucao_otima.append(custos + grafo[posicao_atual][0])
+        return
     
-    if coluna < 0 or coluna > y:
-        return matriz
-    
-    for i in range(x):
-        if matriz[i][coluna] != MAX_INT:
-            matriz[i][coluna] -= valor
-    return matriz
+    for i in range(quant_cidades):
+        if (visitados[i] == False) and grafo[posicao_atual][i]:
+            visitados[i] = True
+            caixeiro_viajante(grafo, visitados, i, quant_cidades, num_cidades_visitadas+1, custos + grafo[posicao_atual][i], solucao_otima)
+            visitados[i] = False
 
-def subtrair_valor_linha(linha: list, valor: int) -> list:
-    return [elem - valor if elem != MAX_INT else elem for elem in linha]
+def branch_and_bound(grafo: list, quant_cidades: int):
+    visitados = [False for _ in range(quant_cidades)]
+    visitados[0] = True
+    solucao_otima = []
+    caixeiro_viajante (grafo, visitados, 0, quant_cidades, 1,  0, solucao_otima)
+    return min(solucao_otima)
 
-def sum_menores_valores_linhas(matriz: list) -> int:
-    menores_valores = []
-    for linha in matriz:
-        menores_valores.append(min(linha))
-    return sum(menores_valores)
+grafo = [
+    [0, 10, 15, 20],
+    [10, 0, 35, 25],
+    [15, 35, 0, 30],
+    [20, 25, 30, 0]
+]
 
-def sum_menores_valores_colunas(matriz: list) -> int:
-    x = len(matriz)
-    y = len(matriz[0])
-    menores_valores = [min(matriz[x][y] for i in range(x)) for j in range(y)]
-    return sum(menores_valores)
-
-def menor_custo(matriz: list) -> int:
-    linhas = sum_menores_valores_linhas(matriz)
-    colunas  = sum_menores_valores_colunas(matriz)
-    return linhas + colunas
+print(f"A menor distância possível para o problema TSP com 4 cidades é: {branch_and_bound(grafo, len(grafo)}")
